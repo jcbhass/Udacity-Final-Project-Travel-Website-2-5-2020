2dbb1d0dc11cb4167e40f6aebc014c94c88ac2a0
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-02-05 09:26:28 -0500

    installed Babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,15 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',	//don’t need quotation marks. 
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+     }
+    
 }
