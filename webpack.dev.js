655403ce56b62f66aa8a84f3555eb144ad58eb28
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-02-06 09:50:51 -0500

    setup cleanwebpackplugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const polyfill = require("@babel/polyfill");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: ["@babel/polyfill", "./src/client/index.js"],
@@ -18,6 +19,15 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+    })
     ]    
 }
